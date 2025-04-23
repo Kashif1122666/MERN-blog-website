21a15a409698833f6b94fe88525d104d48f69f56
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-04-23 13:12:37 +0500

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+
 dotenv.config({});
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
@@ -11,7 +13,17 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use('/api/user', userRoutes);
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/test', (req, res) => {
+  res.json({message:'I am backend !'});
 });
