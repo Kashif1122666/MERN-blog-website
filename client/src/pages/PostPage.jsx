fbfe2b2c91250fa683238e8652650b6819410999
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-05-31 19:51:25 +0500

    add call to action to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import CallToAction from '../components/CallToAction';
 
 const PostPage = () => {
     const {postslug} = useParams();
@@ -48,12 +49,14 @@ const PostPage = () => {
         <img src={post && post.image} alt={post && post.title} className='mt-10 p-3 max-h-[600px] w-full object-cover border border-black' />
         <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
            <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-            <h1>{post && post.whatsapp}</h1>
-           {/* <span className='italic'>{post && (post.content.length / 1000).toFixed(0)}{' '}mins read</span> */}
+            {/* <h1>{post && post.whatsapp}</h1> */}
+           <span className='italic'>{post && (post.content.length / 1000).toFixed(0)}{' '}mins read</span>
         </div>
         <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content }}>            
         </div>
-       
+          <div className='max-w-4xl mx-auto w-full'>
+            <CallToAction/>
+          </div>
     </main>
   )
 }
